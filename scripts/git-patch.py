777c9ed4c89c0c95f507eee2e3c5b2e8e6ac2fa4
Eric <ericncnl3742@gmail.com>
2025-10-16 05:29:36 +0800

    add some addon on base64 decode

--- scripts/git-patch.py
+++ scripts/git-patch.py
@@ -27,11 +27,12 @@ REPORT_TEMPLATE = """
 - Some characters in the crash input and report may not render properly due to encoding issues. Please refer to the raw files for accurate representation.
 """
 
-def remove_ansi_escape_sequences(text):
 
+def remove_ansi_escape_sequences(text):
 
     ansi_escape = re.compile(r"\x1B[@-_][0-?]*[ -/]*[@-~]")
     return ansi_escape.sub("", text)
+
 
 def base64_decode(data):
     """Decode base64, padding being optional.
@@ -40,6 +41,9 @@ def base64_decode(data):
     :returns: The decoded byte string.
 
     """
+    missing_padding = len(data) % 4
+    if missing_padding:
+        data += "=" * (4 - missing_padding)
     return base64.b64decode(data)
 
 
@@ -99,7 +103,9 @@ def create_patch_branch(file_path, repo_path, patches_dir, base_branch):
     report_content = REPORT_TEMPLATE.format(
         patch_id=file_path.split(".")[0],
         crash_input=base64_decode(data["input"]).decode("utf-8", errors="replace"),
-        crash_report=remove_ansi_escape_sequences(base64_decode(data["report"]).decode("utf-8", errors="replace")),
+        crash_report=remove_ansi_escape_sequences(
+            base64_decode(data["report"]).decode("utf-8", errors="replace")
+        ),
         fix_description=data["history"][-1]["reason"],
     )
 
